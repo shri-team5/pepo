972f648fc805b09021919c91cde768f42bc5ee1f
Grimfri <destiny.mail0@gmail.com>
2016-08-16 01:09:51 +0300

    Добавил 'api' к блоку userpic, Переместил дату твита в правый верхний угол

--- common.blocks/tweet/tweet.bemtree.js
+++ common.blocks/tweet/tweet.bemtree.js
@@ -1,31 +1,33 @@
 block('tweet').content()(function() {
     var data = this.ctx.tweetData;
 
-    console.log(new Date(data.created_at));
     return [
         {
             block: 'userpic',
-            attrs: {
-                src: '//'+data.user.avatar,
-                alt: data.user.login
-            },
+            src: '//'+data.user.avatar,
+            alt: data.user.login,
             mix: { block: 'tweet', elem: 'userpic' }
         },
         {
             elem: 'content',
             content:[
                 {
-                    block: 'username',
-                    content: data.user.name,
-                    mix: { block: 'tweet', elem: 'username' }
+                    elem:'head',
+                    content:[
+                        {
+                            block: 'username',
+                            content: data.user.name,
+                            mix: { block: 'tweet', elem: 'username' }
+                        },
+                        {
+                            elem: 'created',
+                            content: (new Date(data.created_at)).toString()
+                        }
+                    ]
                 },
                 {
                     elem: 'text',
                     content: data.text
-                },
-                {
-                    elem: 'created',
-                    content: (new Date(data.created_at)).toString()
                 }
             ]
         }
