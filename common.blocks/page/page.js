270a2fe7e32d1dfd8b4a4ff6bc64fb39b4b49cdf
Vassily Krasnov <mykrasnov@gmail.com>
2016-08-22 13:32:31 +0300

    Порефакторить блок page

--- common.blocks/page/page.js
+++ common.blocks/page/page.js
@@ -1,11 +1,11 @@
 modules.define('page', ['i-bem__dom'], function(provide, BEMDOM) {
 
-    provide(BEMDOM.decl(this.name, {
-        onSetMod: {
-            js: function () {
-                console.log('Root page block');
-            }
+provide(BEMDOM.decl(this.name, {
+    onSetMod: {
+        js: function () {
+            console.log('Root page block');
         }
-    })); // Объявляем базовый блок
+    }
+})); // Объявляем базовый блок
 
 });
