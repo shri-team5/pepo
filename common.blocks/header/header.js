d0fd9bd4be80c1cb8a2d4f768c3d233a4f2aab0b
Vassily Krasnov <mykrasnov@gmail.com>
2016-08-24 00:17:38 +0300

    Добавить функциональность в header

--- common.blocks/header/header.js
+++ common.blocks/header/header.js
@@ -4,12 +4,31 @@ modules.define('header', ['i-bem__dom'], function (provide, BEMDOM) {
             onSetMod: {
                 js: function () {
                     this.elem('plus').on('click', this._onNewTweetClick.bind(this));
+                    this.elem('back').on('click', this._onBackClick.bind(this));
+                    this.elem('submit').on('click', this._onSubmitClick.bind(this));
                 }
             },
+
+            _onSubmitClick: function () {
+                this.emit('header-submit');
+            },
+
+            _onBackClick: function () {
+                this.emit('header-back');
+                this.toggleMod(this.elem('back'), 'visible');
+                this.setMod(this.elem('plus'), 'visible', true);
+
+                this.setMod(this.elem('burger'), 'visible', true);
+                this.setMod(this.elem('submit'), 'visible', false);
+            },
+
             _onNewTweetClick: function () {
-                console.log('emit');
-                this.emit('new-tweet');
-                this.setMod('type', 'new');
+                this.emit('header-new-tweet');
+                this.setMod(this.elem('back'), 'visible', true);
+                this.setMod(this.elem('plus'), 'visible', false);
+
+                this.setMod(this.elem('burger'), 'visible', false);
+                this.setMod(this.elem('submit'), 'visible', true);
             }
         })
     )
