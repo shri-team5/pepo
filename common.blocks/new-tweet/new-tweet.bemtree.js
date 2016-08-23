e6447514d556dbd55cd1c4d1ed053036cd9e6240
Vassily Krasnov <mykrasnov@gmail.com>
2016-08-24 00:18:56 +0300

    Добавить стили и разметку для блока-формы нового твита

--- common.blocks/new-tweet/new-tweet.bemtree.js
+++ common.blocks/new-tweet/new-tweet.bemtree.js
@@ -1,10 +1,11 @@
 block('new-tweet').content()(function() {
     return [
         {
-            elem: 'input',
-            content: {
-                block: 'input'
-            }
+            block: 'textarea',
+            mix: [ { block: 'new-tweet', elem: 'input' } ],
+            name: 'text',
+            mods: { theme : 'islands', size : 'xl', width : 'available' },
+            placeholder: 'Текст твита'
         },
         {
             elem: 'counter'
