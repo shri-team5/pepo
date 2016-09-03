2fabce1fed5b72497ccaa9ecf4edc8b74685b8c5
Grimfri <destiny.mail0@gmail.com>
2016-09-03 15:32:58 +0300

    Валидация поля для ввода сообщения

--- common.blocks/new-tweet/new-tweet.bemtree.js
+++ common.blocks/new-tweet/new-tweet.bemtree.js
@@ -4,6 +4,7 @@ block('new-tweet').content()(function() {
             block: 'textarea',
             mix: [ { block: 'new-tweet', elem: 'input' } ],
             name: 'text',
+            maxLength: 140,
             mods: { theme : 'islands', size : 'xl', width : 'available' },
             placeholder: 'Текст твита'
         },
