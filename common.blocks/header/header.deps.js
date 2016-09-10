9ce9167919ae0dc93ec9bb9268657e16790c4c6f
Vassily Krasnov <mykrasnov@gmail.com>
2016-09-11 02:06:30 +0300

    Заменить символ + на .svg-иконку

--- common.blocks/header/header.deps.js
+++ common.blocks/header/header.deps.js
@@ -5,7 +5,7 @@
         'user-menu',
         {
             block: 'icon',
-            mods: {type: ['back1', 'back2', 'send1', 'send2', 'send3', 'send4']}
+            mods: {type: ['plus', 'back1', 'back2', 'send1', 'send2', 'send3', 'send4']}
         },
         {elems: ['plus', 'back', 'burger', 'submit']}
     ]
