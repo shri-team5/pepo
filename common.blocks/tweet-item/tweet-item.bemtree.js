c47e5b6a1d2d257f996a302abf4fd7f4820ec4fb
grimfri <destiny.mail0@gmail.com>
2016-09-12 00:54:16 +0300

    Изменил отображение элемента твита

--- common.blocks/tweet-item/tweet-item.bemtree.js
+++ common.blocks/tweet-item/tweet-item.bemtree.js
@@ -2,12 +2,7 @@ block('tweet-item').content()(function () {
     const data = this.ctx.data;
 
     return [
-        {
-            block: 'userpic',
-            src: data.author.avatarPath,
-            alt: data.author.username,
-            mix: {block: 'tweet-item', elem: 'userpic'}
-        },
+        
         {
             elem: 'content',
             content: [
@@ -15,25 +10,36 @@ block('tweet-item').content()(function () {
                     elem: 'head',
                     content: [
                         {
-                            block: 'username',
-                            content: data.author.fullName,
-                            href: "/profile/" + data.author._id,
-                            title: '@' + data.author.username,
-                            mix: {block: 'tweet-item', elem: 'username'}
+                            block: 'userpic',
+                            src: data.author.avatarPath,
+                            alt: data.author.username,
+                            mix: {block: 'tweet-item', elem: 'userpic'}
                         },
                         {
-                            elem: 'created',
-                            content: {
-                                block: 'time',
-                                time: data.createdAt,
-                                relative: true
-                            }
-                        },
-                        {
-                            elem: 'reply',
-                            attrs: {
-                                href: '/tweets/' + data.parentTweet
-                            }
+                            elem:'head-info',
+                            content:[
+                                {
+                                    block: 'username',
+                                    content: data.author.fullName,
+                                    href: "/profile/" + data.author._id,
+                                    title: '@' + data.author.username,
+                                    mix: {block: 'tweet-item', elem: 'username'}
+                                },
+                                {
+                                    elem: 'created',
+                                    content: {
+                                        block: 'time',
+                                        time: data.createdAt,
+                                        relative: true
+                                    }
+                                },
+                                {
+                                    elem: 'reply',
+                                    attrs: {
+                                        href: '/tweets/' + data.parentTweet
+                                    }
+                                }
+                            ]
                         }
                     ]
                 },
