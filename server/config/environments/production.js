1d2cb389701e895503a9b7436d1d4bd403cf5153
Artur <destiny.mail0@gmail.com>
2016-09-14 11:53:08 +0300

    Вернул отображение твитов как раньше, прописал домен (#178)

--- server/config/environments/production.js
+++ server/config/environments/production.js
@@ -18,12 +18,12 @@ module.exports = {
         facebookAuth : {
             clientID      : 1733169776932912, // your App ID
             clientSecret  : '51a16703cd414945dbbdedf984e05e2b', // your App Secret
-            callbackURL   : 'http://188.166.17.158/auth/facebook/callback'
+            callbackURL   : 'http://pepos.pw/auth/facebook/callback'
         },
         vkontakteAuth : {
             clientID      : 5607195, // your App ID
             clientSecret  : 'xmhbcYmU1c3gxLPwfTaK', // your App Secret
-            callbackURL   : 'http://188.166.17.158/auth/vkontakte/callback'
+            callbackURL   : 'http://pepos.pw/auth/vkontakte/callback'
         }
     }
 };
