2b84b5ace94b7b275f71590335e123a708c500fa
Artur <destiny.mail0@gmail.com>
2016-09-18 21:47:39 +0300

    Добавил отображение смайликов и перевода строки (#202)

--- common.blocks/root/root.bemtree.js
+++ common.blocks/root/root.bemtree.js
@@ -1,11 +1,13 @@
-block('root').replace()(function() {
+block('root').replace()(function () {
     var ctx = this.ctx,
         i18n = this.i18n,
         data = this.data = ctx.data,
         meta = data.meta || {},
         og = meta.og || {};
 
-    if (ctx.context) return ctx.context;
+    if (ctx.context) {
+        return ctx.context;
+    }
 
     return {
         block: 'page',
@@ -20,26 +22,29 @@ block('root').replace()(function() {
                 elem: 'css',
                 url: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700&subset=cyrillic,cyrillic-ext'
             },
-            { elem : 'css', url : '/icon_128.png' , attrs : { rel : 'icon' } },
-            { elem : 'css', url : '/icon_200.png' , attrs : { rel : 'apple-touch-icon' } }
+            {
+                elem: 'css',
+                url: '//cdn.jsdelivr.net/emojione/2.2.6/assets/css/emojione.min.css'
+            },
+            {elem: 'css', url: '/icon_128.png', attrs: {rel: 'icon'}},
+            {elem: 'css', url: '/icon_200.png', attrs: {rel: 'apple-touch-icon'}}
         ],
-        scripts: [ 
+        scripts: [
             {
                 elem: 'js',
                 url: '/index.' + i18n('root', 'lang') + '.min.js'
             }
-            
         ],
         head: [
-            { elem: 'meta', attrs: { name: 'mobile-web-app-capable', content: 'yes' } },
-            { elem: 'meta', attrs: { name: 'apple-mobile-web-app-capable', content: 'yes' } },
-            { elem: 'meta', attrs: { name: 'description', content: meta.description } },
-            { elem: 'meta', attrs: { property: 'og:title', content: og.title || data.title } },
-            { elem: 'meta', attrs: { property: 'og:url', content: og.url } },
-            { elem: 'meta', attrs: { property: 'og:site_name', content: og.siteName } },
-            { elem: 'meta', attrs: { property: 'og:locale', content: og.locale || 'en_US' } },
-            { elem: 'meta', attrs: { property: 'og:type', content: 'website' } },
-            { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } }
+            {elem: 'meta', attrs: {name: 'mobile-web-app-capable', content: 'yes'}},
+            {elem: 'meta', attrs: {name: 'apple-mobile-web-app-capable', content: 'yes'}},
+            {elem: 'meta', attrs: {name: 'description', content: meta.description}},
+            {elem: 'meta', attrs: {property: 'og:title', content: og.title || data.title}},
+            {elem: 'meta', attrs: {property: 'og:url', content: og.url}},
+            {elem: 'meta', attrs: {property: 'og:site_name', content: og.siteName}},
+            {elem: 'meta', attrs: {property: 'og:locale', content: og.locale || 'en_US'}},
+            {elem: 'meta', attrs: {property: 'og:type', content: 'website'}},
+            {elem: 'meta', attrs: {name: 'viewport', content: 'width=device-width, initial-scale=1'}}
 
         ],
         mods: {
